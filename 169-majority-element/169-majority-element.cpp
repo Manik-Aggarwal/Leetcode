6b9b04122e2706b8696a44efe80ba42a6ca93429
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-02-21 12:23:36 +0530

    Time: 19 ms (79.70%), Space: 19.7 MB (52.27%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     //by using hashmap
-    int majorityElement(vector<int>& nums) {
-        unordered_map <int,int> m;
-        int ans;
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map <int,int> m;
+//         int ans;
         
-        for(int i=0; i<nums.size(); i++) m[nums[i]]++;
+//         for(int i=0; i<nums.size(); i++) m[nums[i]]++;
         
-        for(int i=0; i<nums.size(); i++){
-            if(m[nums[i]] > nums.size()/2) ans = nums[i];
-        }
-        return ans;
+//         for(int i=0; i<nums.size(); i++){
+//             if(m[nums[i]] > nums.size()/2) ans = nums[i];
+//         }
+//         return ans;
+//     }
+    
+    //by Moore's voting algo
+     int majorityElement(vector<int>& nums) {
+        int count = 0;
+         int elem = 0;
+         
+         for(int num : nums){
+             if(count==0) elem = num;
+             
+             if(num == elem) count++;
+             
+             else count--;
+         }
+         
+         return elem;
     }
 };
