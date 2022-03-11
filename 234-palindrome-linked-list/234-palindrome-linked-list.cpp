8e97a0a1f87ad5df3dac92f7620bc0642dea1e2b
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-03-11 18:26:10 +0530

    Time: 409 ms (25.07%), Space: 118 MB (49.75%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -9,22 +9,74 @@
  * };
  */
 class Solution {
-private:
-    bool checkPalindrome(vector<int> arr){
-        int n = arr.size();
-        for(int i=0, j=n-1; i<j; i++,j--){
-            if(arr[i] != arr[j]) return false;
+    //Approach 1
+// private:
+//     bool checkPalindrome(vector<int> arr){
+//         int n = arr.size();
+//         for(int i=0, j=n-1; i<j; i++,j--){
+//             if(arr[i] != arr[j]) return false;
+//         }
+//         return true;
+//     }
+// public:
+//     bool isPalindrome(ListNode* head) {
+//         ListNode* temp = head;
+//         vector<int> arr;
+//         while(temp){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//         return checkPalindrome(arr);
+//     }
+    
+//Approach 2
+    private:
+        ListNode* getMid(ListNode* head){
+            ListNode* slow = head;
+            ListNode*fast = head->next;
+            
+            while(fast!=NULL && fast->next!=NULL){
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+            return slow;
         }
-        return true;
-    }
-public:
-    bool isPalindrome(ListNode* head) {
-        ListNode* temp = head;
-        vector<int> arr;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
+    
+        ListNode* reverse(ListNode* head){
+            ListNode* curr = head;
+            ListNode* prev = NULL;
+            ListNode* nex = NULL;
+            
+            while(curr){
+                nex = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = nex;
+            }
+            return prev;
         }
-        return checkPalindrome(arr);
-    }
+    
+    public:
+        bool isPalindrome(ListNode* head) {
+            if(head == NULL || head->next == NULL) return true;
+            ListNode* mid = getMid(head);
+            
+            ListNode* temp = mid->next;
+            
+            mid->next = reverse(temp);
+            
+            ListNode* h1 = head;
+            ListNode* h2 = mid->next;
+            while(h2){
+                if(h1->val != h2->val) return false;
+                h1 = h1->next;
+                h2 = h2->next;
+            }
+            
+            // making LL as it was
+            temp = mid->next;
+            mid->next = reverse(temp);
+            
+            return true;
+        }
 };
