45c894096298ee74490561acde4d15c2f5f2bf1b
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-08-24 17:00:12 +0530

    Time: 4 ms (28.68%), Space: 6.4 MB (52.99%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -45,7 +45,7 @@
 
 //         for(int i=0;i<m;i++){
 //             for(int j=0;j<n;j++){
-//                 if(i==0||j==0) dp[i][j]=1;
+//                 if(i==0 && j==0) dp[i][j]=1;
 //                 else{
 //                     int up = 0;
 //                     int left = 0;
@@ -69,7 +69,7 @@ public:
             vector<int> curr(n,0);
             
             for(int j=0;j<n;j++){
-                if(i==0||j==0) curr[j]=1;
+                if(i==0 && j==0) curr[j]=1;
                 else{
                     int up = 0;
                     int left = 0;
