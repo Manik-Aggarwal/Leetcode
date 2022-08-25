8fec2576b9317447687068556b35d173aa63ad75
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-08-25 13:32:28 +0530

    Time: 16 ms (84.77%), Space: 9.9 MB (71.12%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -58,33 +58,66 @@
 // };
 
 //////////////////////////////////Tabulation////////////////////////////
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//         vector<vector<int>> dp(m,vector<int>(n,0));
+        
+//         for(int j=0; j<n; j++) dp[0][j] = matrix[0][j];
+        
+//         for(int i=1; i<m; i++){
+//             for(int j=0; j<n; j++){
+//                 int up = matrix[i][j] + dp[i-1][j];
+                
+//                 int leftDiagonal = matrix[i][j];
+//                 if(j-1 >= 0) leftDiagonal += dp[i-1][j-1];
+//                 else leftDiagonal += 1e9;
+                
+//                 int rightDiagonal = matrix[i][j];
+//                 if(j+1 < n) rightDiagonal += dp[i-1][j+1];
+//                 else rightDiagonal += 1e9;
+                
+//                 dp[i][j] = min(up,min(leftDiagonal,rightDiagonal));
+//             }
+//         }
+        
+//         int mini = INT_MAX;
+//         for(int j=0; j<n; j++) mini = min(mini,dp[n-1][j]);
+//         return mini;
+//     }
+// };
+
+//////////////////////////////Space Optimisation////////////////////////////
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        vector<int> prev(n,0), curr(m,0);
         
-        for(int j=0; j<n; j++) dp[0][j] = matrix[0][j];
+        for(int j=0; j<n; j++) prev[j] = matrix[0][j]; //here prev make sure
         
         for(int i=1; i<m; i++){
             for(int j=0; j<n; j++){
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
                 
                 int leftDiagonal = matrix[i][j];
-                if(j-1 >= 0) leftDiagonal += dp[i-1][j-1];
+                if(j-1 >= 0) leftDiagonal += prev[j-1];
                 else leftDiagonal += 1e9;
                 
                 int rightDiagonal = matrix[i][j];
-                if(j+1 < n) rightDiagonal += dp[i-1][j+1];
+                if(j+1 < n) rightDiagonal += prev[j+1];
                 else rightDiagonal += 1e9;
                 
-                dp[i][j] = min(up,min(leftDiagonal,rightDiagonal));
+                curr[j] = min(up,min(leftDiagonal,rightDiagonal));
             }
+            prev = curr;
         }
         
         int mini = INT_MAX;
-        for(int j=0; j<n; j++) mini = min(mini,dp[n-1][j]);
+        for(int j=0; j<n; j++) mini = min(mini,prev[j]);
         return mini;
     }
 };
