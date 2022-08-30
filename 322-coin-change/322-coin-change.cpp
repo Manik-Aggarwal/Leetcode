ffe17413bcb914e897e173f1f300a3c0b1a98f7b
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-08-30 12:18:08 +0530

    Time: 93 ms (85.04%), Space: 18.7 MB (33.61%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -51,63 +51,59 @@
 // };
 
 ///////////////////////////////Tabulation////////////////////////////////////////
+// class Solution {
+// public:    
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int> (amount+1,0));
+        
+//         for(int tar = 0; tar<=amount; tar++){
+//             if(tar%coins[0] == 0) dp[0][tar] = tar/coins[0];
+//             else dp[0][tar] = 1e9;
+//         }
+        
+//         for(int ind = 1; ind<n; ind++){
+//             for(int tar = 0; tar<=amount; tar++){
+//                 int notTake = 0 + dp[ind-1][tar];
+//                 int take = 1e9;
+//                 if(coins[ind] <= tar) take = 1 + dp[ind][tar-coins[ind]];
+
+//                 dp[ind][tar] = min(take, notTake);
+//             }
+//         }
+        
+//         int ans = dp[n-1][amount];
+//         if(ans >= 1e9) return -1;
+//         else return ans;
+//     }
+// };
+
+///////////////////////////////////Space Optimisation////////////////////////////
 class Solution {
-public:
-    int f(int ind, vector<int> &coins, int tar, vector<vector<int>> &dp){
-        if(ind == 0){
-            if(tar%coins[0] == 0) return tar/coins[0];
-            else return 1e9;
-        }
-        if(dp[ind][tar] != -1) return dp[ind][tar];
-        
-        int notTake = 0 + f(ind-1, coins, tar,dp);
-        int take = 1e9;
-        if(coins[ind] <= tar) take = 1 + f(ind, coins, tar-coins[ind],dp);
-        
-        return dp[ind][tar] = min(take, notTake);
-    }
-    
+public:    
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1,0));
+        vector<int> prev(amount+1,0);
+        vector<int> curr(amount+1.0);
         
         for(int tar = 0; tar<=amount; tar++){
-            if(tar%coins[0] == 0) dp[0][tar] = tar/coins[0];
-            else dp[0][tar] = 1e9;
+            if(tar%coins[0] == 0) prev[tar] = tar/coins[0];
+            else prev[tar] = 1e9;
         }
         
         for(int ind = 1; ind<n; ind++){
             for(int tar = 0; tar<=amount; tar++){
-                int notTake = 0 + dp[ind-1][tar];
+                int notTake = 0 + prev[tar];
                 int take = 1e9;
-                if(coins[ind] <= tar) take = 1 + dp[ind][tar-coins[ind]];
+                if(coins[ind] <= tar) take = 1 + curr[tar-coins[ind]];
 
-                dp[ind][tar] = min(take, notTake);
+                curr[tar] = min(take, notTake);
             }
+            prev = curr;
         }
         
-        int ans = dp[n-1][amount];
+        int ans = prev[amount];
         if(ans >= 1e9) return -1;
         else return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
