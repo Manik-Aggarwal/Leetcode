91ca52dfccdf55f38bb734d9964e785d936aa600
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-08-30 13:21:07 +0530

    Time: 58 ms (35.12%), Space: 18.4 MB (45.69%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -19,24 +19,49 @@
 // };
 
 //////////////////////////////////Memoization////////////////////////////////
-class Solution {
-public:
-    int f(int ind, vector<int> &coins, int tar, vector<vector<int>> &dp){
-        if(ind == 0){
-            return tar%coins[0] == 0;
-        }
-        if(dp[ind][tar] != -1) return dp[ind][tar];
+// class Solution {
+// public:
+//     int f(int ind, vector<int> &coins, int tar, vector<vector<int>> &dp){
+//         if(ind == 0){
+//             return tar%coins[0] == 0;
+//         }
+//         if(dp[ind][tar] != -1) return dp[ind][tar];
         
-        int notTake = 0 + f(ind-1, coins, tar, dp);
-        int take = 0;
-        if(coins[ind] <= tar) take = f(ind, coins, tar-coins[ind], dp);
+//         int notTake = 0 + f(ind-1, coins, tar, dp);
+//         int take = 0;
+//         if(coins[ind] <= tar) take = f(ind, coins, tar-coins[ind], dp);
         
-        return dp[ind][tar] = take + notTake;
-    }
+//         return dp[ind][tar] = take + notTake;
+//     }
     
+//     int change(int amount, vector<int>& coins) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n,vector<int> (amount+1,-1));
+//         return f(n-1, coins, amount, dp);
+//     }
+// };
+
+//////////////////////////////////Tabulation////////////////////////////////
+class Solution {
+public:    
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
-        return f(n-1, coins, amount, dp);
+        vector<vector<int>> dp(n, vector<int> (amount+1,0));
+        
+        for(int tar = 0; tar<=amount; tar++){
+            if(tar%coins[0] == 0) dp[0][tar] = 1;
+        }
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int tar = 0; tar<=amount; tar++){
+                int notTake = 0 + dp[ind-1][tar];
+                int take = 0;
+                if(coins[ind] <= tar) take = dp[ind][tar-coins[ind]];
+
+                dp[ind][tar] = take + notTake;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
