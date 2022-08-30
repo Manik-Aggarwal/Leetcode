aa6a64d202c4bcfdccd817b77b49e468456efa5e
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-08-30 12:12:43 +0530

    Time: 225 ms (19.45%), Space: 45.2 MB (26.51%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -24,6 +24,33 @@
 
 
 //////////////////////////////////Memoization//////////////////////////////////
+// class Solution {
+// public:
+//     int f(int ind, vector<int> &coins, int tar, vector<vector<int>> &dp){
+//         if(ind == 0){
+//             if(tar%coins[0] == 0) return tar/coins[0];
+//             else return 1e9;
+//         }
+//         if(dp[ind][tar] != -1) return dp[ind][tar];
+        
+//         int notTake = 0 + f(ind-1, coins, tar,dp);
+//         int take = 1e9;
+//         if(coins[ind] <= tar) take = 1 + f(ind, coins, tar-coins[ind],dp);
+        
+//         return dp[ind][tar] = min(take, notTake);
+//     }
+    
+//     int coinChange(vector<int>& coins, int amount) {
+//         int n = coins.size();
+//         vector<vector<int>> dp(n, vector<int> (amount+1,-1));
+        
+//         int ans = f(n-1, coins, amount,dp);
+//         if(ans >= 1e9) return -1;
+//         else return ans;
+//     }
+// };
+
+///////////////////////////////Tabulation////////////////////////////////////////
 class Solution {
 public:
     int f(int ind, vector<int> &coins, int tar, vector<vector<int>> &dp){
@@ -42,10 +69,45 @@ public:
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1,-1));
+        vector<vector<int>> dp(n, vector<int> (amount+1,0));
         
-        int ans = f(n-1, coins, amount,dp);
+        for(int tar = 0; tar<=amount; tar++){
+            if(tar%coins[0] == 0) dp[0][tar] = tar/coins[0];
+            else dp[0][tar] = 1e9;
+        }
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int tar = 0; tar<=amount; tar++){
+                int notTake = 0 + dp[ind-1][tar];
+                int take = 1e9;
+                if(coins[ind] <= tar) take = 1 + dp[ind][tar-coins[ind]];
+
+                dp[ind][tar] = min(take, notTake);
+            }
+        }
+        
+        int ans = dp[n-1][amount];
         if(ans >= 1e9) return -1;
         else return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
