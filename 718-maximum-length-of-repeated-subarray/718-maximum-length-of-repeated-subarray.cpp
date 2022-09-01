72b6b994eea1449989735a666ed3494f70e3e1f3
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-01 11:28:36 +0530

    Time: 633 ms (25.51%), Space: 11.9 MB (92.70%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,5 +1,28 @@
 ///////////////////based on longest common subseq/////////////////////
+
 ////////////////////////tabulation///////////////////////////////////
+// class Solution {
+// public:
+//     int findLength(vector<int>& nums1, vector<int>& nums2) {
+//         int n = nums1.size();
+//         int m = nums2.size();
+//         int ans = 0;
+        
+//         vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=m; j++){
+//                 if(nums1[i-1] == nums2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
+//                 else dp[i][j] = 0;
+//                 ans = max(ans, dp[i][j]);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
+
+/////////////////////////////////Space optimisation////////////////////////
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
@@ -7,14 +30,16 @@ public:
         int m = nums2.size();
         int ans = 0;
         
-        vector<vector<int>> dp(n+1, vector<int>(m+1,0));
+        vector<int> prev(n+1,0); 
+        vector<int> curr(m+1,0);
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(nums1[i-1] == nums2[j-1]) dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = 0;
-                ans = max(ans, dp[i][j]);
+                if(nums1[i-1] == nums2[j-1]) curr[j] = 1 + prev[j-1];
+                else curr[j] = 0;
+                ans = max(ans, curr[j]);
             }
+            prev = curr;
         }
         
         return ans;
