2d36153752c3b43b30a1e59018526dbf29890b3a
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-02 12:56:54 +0530

    Time: 19 ms (60.82%), Space: 9.1 MB (23.60%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,25 +1,51 @@
-class Solution {
-public:
-    int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(j<0) return i+1; //if str t exhausted
-        if(i<0) return j+1; //if str s exhausted
-        if(dp[i][j] != -1) return dp[i][j];
+// class Solution {
+// public:
+//     int f(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+//         if(j<0) return i+1; //if str t exhausted
+//         if(i<0) return j+1; //if str s exhausted
+//         if(dp[i][j] != -1) return dp[i][j];
         
-        if(s[i] == t[j]) return dp[i][j] = 0 + f(i-1,j-1,s,t,dp);
-        else{
-            int insert = f(i,j-1,s,t,dp);
-            int delet = f(i-1,j,s,t,dp);
-            int replace = f(i-1,j-1,s,t,dp);
+//         if(s[i] == t[j]) return dp[i][j] = 0 + f(i-1,j-1,s,t,dp);
+//         else{
+//             int insert = f(i,j-1,s,t,dp);
+//             int delet = f(i-1,j,s,t,dp);
+//             int replace = f(i-1,j-1,s,t,dp);
             
-            return dp[i][j] = 1 + min(insert, min(delet,replace));
-        }
-    }
+//             return dp[i][j] = 1 + min(insert, min(delet,replace));
+//         }
+//     }
     
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+//     int minDistance(string word1, string word2) {
+//         int n = word1.size();
+//         int m = word2.size();
+//         vector<vector<int>> dp(n,vector<int>(m,-1));
         
-        return f(n-1,m-1,word1,word2,dp);
+//         return f(n-1,m-1,word1,word2,dp);
+//     }
+// };
+
+class Solution {
+public:    
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        
+        for(int i=0; i<=m; i++) dp[i][0] = i;
+        
+        for(int j=0; j<=n; j++) dp[0][j] = j;
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
+                else{
+                    int insert = dp[i][j-1];
+                    int delet = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+                    
+                    dp[i][j] = 1 + min({insert, delet, replace});
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
