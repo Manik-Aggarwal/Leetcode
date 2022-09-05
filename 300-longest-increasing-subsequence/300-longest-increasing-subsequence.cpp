bae9cb899f23e91bf48d6b645e7138df8a5be8ae
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-05 12:52:10 +0530

    Time: 817 ms (16.84%), Space: 10.8 MB (21.54%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -87,39 +87,72 @@
 // };
 
 /////////////////////////////********************///////////////////////////////////////
+// class Solution {
+// public:
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> dp(n,1);
+//         int maxi = 1;
+        
+//         for(int ind = 0; ind<n; ind++){
+//             for(int prevInd = 0; prevInd<ind; prevInd++){
+//                 if(nums[ind] > nums[prevInd]) dp[ind] = max(dp[ind], 1+dp[prevInd]);
+//             }
+            
+//             maxi = max(maxi,dp[ind]);
+//         }
+        
+//         return maxi;
+//     }
+// };
+
+
+//////////////////printing lis///////////////////////
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,1);
-        int maxi = 1;
-        
-        for(int ind = 0; ind<n; ind++){
-            for(int prevInd = 0; prevInd<ind; prevInd++){
-                if(nums[ind] > nums[prevInd]) dp[ind] = max(dp[ind], 1+dp[prevInd]);
+        vector<int> hash(n,1);
+
+        for(int i=0; i<=n-1; i++){
+            hash[i] = i;
+            for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+
+                if(nums[prev_index]<nums[i] && 1 + dp[prev_index] > dp[i]){
+                    dp[i] = 1 + dp[prev_index];
+                    hash[i] = prev_index;
+                }
             }
-            
-            maxi = max(maxi,dp[ind]);
         }
+
+        int ans = -1;
+        int lastIndex =-1;
         
-        return maxi;
+        //find max from dp array and index of max elem
+        for(int i=0; i<=n-1; i++){
+            if(dp[i]> ans){
+                ans = dp[i];
+                lastIndex = i;
+            }
+        }
+
+        vector<int> temp;
+        temp.push_back(nums[lastIndex]);
+
+        while(hash[lastIndex] != lastIndex){
+            lastIndex = hash[lastIndex];
+            temp.push_back(nums[lastIndex]);    
+        }
+
+        reverse(temp.begin(),temp.end());
+        
+        for(int i=0; i<temp.size(); i++) std::cout<<temp[i]<<" ";
+        std::cout<<endl;
+
+        return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
