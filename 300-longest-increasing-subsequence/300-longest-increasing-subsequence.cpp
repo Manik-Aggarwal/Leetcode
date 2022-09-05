48cfea4bca575cf48d13151dc0c29aa42f35213c
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-05 12:00:14 +0530

    Time: 1548 ms (9.78%), Space: 292.1 MB (11.34%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -18,24 +18,46 @@
 // };
 
 /////////////////////////////Memoization///////////////////////////////////////
+// class Solution {
+// public:
+//     int f(int ind, int prevInd, vector<int> &nums, int n,vector<vector<int>> &dp){
+//         if(ind == n) return 0;
+//         if(dp[ind][prevInd+1] != -1) return dp[ind][prevInd+1];
+        
+//         int notTake = f(ind+1,prevInd,nums,n,dp);
+        
+//         int take = 0;
+//         if(prevInd == -1 || nums[ind]>nums[prevInd]) take = 1 + f(ind+1,ind,nums,n,dp);
+        
+//         return dp[ind][prevInd+1] = max(take,notTake);
+//     }
+    
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        
+//         return f(0,-1,nums,n,dp);
+//     }
+// };
+
+/////////////////////////////Memoization///////////////////////////////////////
 class Solution {
 public:
-    int f(int ind, int prevInd, vector<int> &nums, int n,vector<vector<int>> &dp){
-        if(ind == n) return 0;
-        if(dp[ind][prevInd+1] != -1) return dp[ind][prevInd+1];
-        
-        int notTake = f(ind+1,prevInd,nums,n,dp);
-        
-        int take = 0;
-        if(prevInd == -1 || nums[ind]>nums[prevInd]) take = 1 + f(ind+1,ind,nums,n,dp);
-        
-        return dp[ind][prevInd+1] = max(take,notTake);
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
+        vector<vector<int>> dp(n+1, vector<int>(n+1,0));
         
-        return f(0,-1,nums,n,dp);
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int prevInd = ind-1; prevInd>=-1; prevInd--){
+                int notTake = dp[ind+1][prevInd+1];
+        
+                int take = 0;
+                if(prevInd == -1 || nums[ind]>nums[prevInd]) take = 1 + dp[ind+1][ind+1];
+                
+                dp[ind][prevInd+1] = max(take,notTake);
+            }
+        }
+        
+        return dp[0][-1+1];
     }
 };
