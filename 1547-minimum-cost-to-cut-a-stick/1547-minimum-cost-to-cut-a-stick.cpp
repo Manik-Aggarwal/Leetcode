c13bd93f5f1f89d6b18edc3fbeda09d664f5ed63
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-07 12:18:58 +0530

    Time: 150 ms (30.34%), Space: 10.4 MB (39.82%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,26 +1,54 @@
+// class Solution {
+// 	public:
+// 		int f(int i,int j,vector<int>& cuts,vector<vector<int>>& dp){
+// 			if(i>j) return 0;
+// 			if(dp[i][j]!=-1) return dp[i][j];
+            
+// 			int mini=1e9;
+// 			for(int ind=i;ind<=j;ind++){
+// 				int cost= cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
+// 				mini=min(mini,cost);
+// 			}
+            
+// 			return dp[i][j]=mini;
+// 		}
+
+// 		int minCost(int n, vector<int>& cuts) {
+//             int m=cuts.size();
+// 			vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
+            
+// 			cuts.insert(cuts.begin(),0);
+// 			cuts.push_back(n);
+// 			sort(cuts.begin(),cuts.end());
+			
+// 			return f(1,m,cuts,dp);
+// 		}
+// };
+
 class Solution {
 	public:
-		int f(int i,int j,vector<int>& cuts,vector<vector<int>>& dp){
-			if(i>j) return 0;
-			if(dp[i][j]!=-1) return dp[i][j];
-            
-			int mini=1e9;
-			for(int ind=i;ind<=j;ind++){
-				int cost= cuts[j+1]-cuts[i-1]+f(i,ind-1,cuts,dp)+f(ind+1,j,cuts,dp);
-				mini=min(mini,cost);
-			}
-            
-			return dp[i][j]=mini;
-		}
-
 		int minCost(int n, vector<int>& cuts) {
             int m=cuts.size();
-			vector<vector<int>> dp(m+1,vector<int>(m+1,-1));
+			vector<vector<int>> dp(m+2,vector<int>(m+2,0));
             
 			cuts.insert(cuts.begin(),0);
 			cuts.push_back(n);
 			sort(cuts.begin(),cuts.end());
 			
-			return f(1,m,cuts,dp);
+            for(int i=m; i>=1; i--){
+                for(int j=1; j<=m; j++){
+                    if(i>j) continue;
+                    int mini = 1e9;
+                    
+                    for(int ind=i;ind<=j;ind++){
+                        int cost= cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                        mini=min(mini,cost);
+                    }
+                    
+                    dp[i][j] = mini;
+                }
+            }
+            
+            return dp[1][m];
 		}
-	};
+};
