ac2a0b9036da1c63f0c98d91740ad2baf073e470
Manik Aggarwal <76563109+Manik-Aggarwal@users.noreply.github.com>
2022-09-24 13:38:47 +0530

    Time: 11 ms (94.36%), Space: 21 MB (54.47%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -9,29 +9,53 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//////////////////////////////////O(N^2)/////////////////////////////////////
+// class Solution {
+// public:
+//     int maxDepth(TreeNode* root){
+//         if(!root) return 0;
+        
+//         int lh = maxDepth(root->left);
+//         int rh = maxDepth(root->right);
+        
+//         return 1 + max(lh,rh);
+//     }
+    
+//     bool isBalanced(TreeNode* root) {
+//         if(!root) return true;
+//         int lh = maxDepth(root->left);
+//         int rh = maxDepth(root->right);
+        
+//         if(abs(rh - lh) > 1) return false;
+        
+//         bool leftchild = isBalanced(root->left);
+//         bool rightChild = isBalanced(root->right);
+        
+//         if(!leftchild || !rightChild) return false;
+        
+//         return true;
+//     }
+// };
+
+////////////////////////O(N)//////////////////////////////
 class Solution {
 public:
-    int maxDepth(TreeNode* root){
-        if(!root) return 0;
+    int solve(TreeNode *root){
+        if (!root) return 0;
         
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
+        int leftHeight = solve(root -> left);
+        if (leftHeight == -1) return -1;
         
-        return 1 + max(lh,rh);
+        int rightHeight = solve(root -> right);
+        if (rightHeight == -1) return -1;
+        
+        if (abs(leftHeight - rightHeight) > 1)  return -1;
+        
+        return max(leftHeight, rightHeight) + 1;
     }
     
-    bool isBalanced(TreeNode* root) {
-        if(!root) return true;
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
-        
-        if(abs(rh - lh) > 1) return false;
-        
-        bool leftchild = isBalanced(root->left);
-        bool rightChild = isBalanced(root->right);
-        
-        if(!leftchild || !rightChild) return false;
-        
-        return true;
+    bool isBalanced(TreeNode *root) {
+        return solve(root) != -1;
     }
 };
